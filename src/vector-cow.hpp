f7f896d64eaf52bb155fa6aed76ed7290b296113
Ivan <ivanreshetnikov235@gmail.com>
2024-04-14 15:40:02 +0700

    add rule of 5 methods version1 and initializer_list constructor and some methods

--- src/vector-cow.hpp
+++ src/vector-cow.hpp
@@ -2,6 +2,8 @@
 
 #include <cstddef>
 #include <initializer_list>
+#include <memory>
+#include <utility>
 
 namespace vector_cow {
 
@@ -16,18 +18,64 @@ public:
   using const_pointer = const value_type *;
 
   Vector() = default;
-  Vector(const Vector &other);
-  Vector(Vector &&other);
-  Vector &operator=(const Vector &other);
-  Vector &operator=(Vector &&other);
-  ~Vector();
+  Vector(const Vector &other)
+      : begin_(other.begin_), size_(other.size_), capacity_(other.capacity_) {}
 
-  Vector(size_type size);
-  Vector(size_type size, const_reference value);
-  Vector(std::initializer_list<T> other);
+  Vector(Vector &&other)
+      : begin_(other.begin_), size_(other.size_), capacity_(other.capacity_) {
+    other.begin_ = nullptr;
+    other.size_ = other.capacity_ = 0;
+  }
+
+  Vector &operator=(const Vector &other) {
+    if (this != &other) {
+      other.begin_ = this->begin_;
+      other.size_ = this->size_;
+      other.capacity_ = this->capacity_;
+    }
+    return *this;
+  }
+
+  Vector &operator=(Vector &&other) {
+    Vector tmp(std::move(other));
+    tmp.swap(*this);
+    return *this;
+  };
+
+  ~Vector() {
+    for (auto current = begin_; current != begin_ + size_; ++current) {
+      current->~T();
+    }
+    operator delete(begin_);
+  };
+
+  // explicit Vector(size_type sizes)
+  //     : begin_(static_cast<T *>(operator new(sizeof(T) * sizes))),
+  //     size_(sizes),
+  //       capacity_(sizes) {}
+
+  // Vector(size_type size, const_reference value);
+
+  explicit Vector(std::initializer_list<T> items)
+      : begin_(static_cast<T *>(operator new(sizeof(T) * items.size()))),
+        size_(items.size()), capacity_(size_) {
+    std::uninitialized_move(items.begin(), items.end(), begin_);
+  }
   //   Vector(iterator first, iterator end);
 
+  pointer begin() { return begin_; }
+  pointer end() { return begin_ + size_; }
+  size_type size() const { return size_; }
+  size_type capacity() const { return capacity_; }
+
 private:
+  void detach();
+  void swap(Vector &obj) noexcept {
+    obj.begin_ = this->begin_;
+    obj.size_ = this->size_;
+    obj.capacity_ = this->capacity_;
+  }
+
   pointer begin_ = nullptr;
   size_type size_ = 0;
   size_type capacity_ = 0;
--- test/vec-cow-test.cpp
+++ test/vec-cow-test.cpp
@@ -2,6 +2,12 @@
 
 #include <gtest/gtest.h>
 
+#include <iostream>
+
+TEST(test_vector, initializer_constructor) {
+  vector_cow::Vector<int> v{1, 2, 3};
+}
+
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
